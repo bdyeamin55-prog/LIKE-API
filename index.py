5fa7f05fa547f848a53f026d1987a020128854f5
bdyeamin55-prog <bdyeamin55@gmail.com>
2026-02-01 12:48:05 +0600

    index.py

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
 from wsgi import app
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "এপিআই সার্ভার সফলভাবে চলছে!"
+
+@app.route('/api/hello')
+def hello():
+    return jsonify({"status": "success", "message": "গিটহাব থেকে এপিআই কাজ করছে"})
+
+# ভারসেলের জন্য এটি জরুরি
+def handler(event, context):
+    return app(event, context)
